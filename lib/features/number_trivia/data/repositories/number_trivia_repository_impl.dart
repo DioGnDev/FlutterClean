d9453be414d05233da3a83cd9213816552293ba6
Ilham Hadi Prabawa <guitardio90@gmail.com>
2022-06-23 22:42:58 +0700

    testing remote data source

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -1,13 +1,27 @@
-import 'package:number_trivia_app/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:number_trivia_app/features/number_trivia/data/datasource/number_trivia_local_data_source.dart';
+import 'package:number_trivia_app/features/number_trivia/data/datasource/number_trivia_remote_datasource.dart';
 import 'package:number_trivia_app/features/number_trivia/domain/entity/number_trivia.dart';
 import 'package:dartz/dartz.dart';
 import 'package:number_trivia_app/features/number_trivia/domain/repository/number_trivia_repository.dart';
 import 'package:number_trivia_app/triviakit/error/failures.dart';
+import 'package:number_trivia_app/triviakit/platform/network_info.dart';
 
 class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
+  final NumberTriviaRemoteDataSource remoteDataSource;
+  final NumberTriviaLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  NumberTriviaRepositoryImpl({
+    required this.remoteDataSource,
+    required this.localDataSource,
+    required this.networkInfo,
+  });
+
   @override
-  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia({int? number}) {
-    throw UnimplementedError();
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(
+      {int? number}) async {
+    networkInfo.isConnected;
+    return Right(await remoteDataSource.getConcreteNumberTrivia(number));
   }
 
   @override
--- test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
+++ test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
@@ -1,8 +1,11 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:number_trivia_app/features/number_trivia/data/datasource/number_trivia_local_data_source.dart';
 import 'package:number_trivia_app/features/number_trivia/data/datasource/number_trivia_remote_datasource.dart';
+import 'package:number_trivia_app/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:number_trivia_app/features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
+import 'package:number_trivia_app/features/number_trivia/domain/entity/number_trivia.dart';
 import 'package:number_trivia_app/triviakit/platform/network_info.dart';
 
 class MockRemoteDataSource extends Mock
@@ -26,6 +29,52 @@ void main() {
       remoteDataSource: mockRemoteDataSource,
       localDataSource: mockLocalDataSource,
       networkInfo: mockNetworkInfo,
+    );
+  });
+
+  group('get concrete number trivia', () {
+    final int tNumber = 1;
+    final tNumberTriviaModel =
+        NumberTriviaModel(number: 1, message: "test trivia");
+    final NumberTrivia numberTrivia = tNumberTriviaModel;
+
+    test('should check if the device is online', () {
+      //arrange
+      when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+      //act
+      repository.getConcreteNumberTrivia(number: tNumber);
+      //assert
+      verify(() => mockNetworkInfo.isConnected);
+    });
+  });
+
+  group('device is online', () {
+    final int tNumber = 1;
+    final tNumberTriviaModel =
+        NumberTriviaModel(number: 1, message: "test trivia");
+    final NumberTrivia numberTrivia = tNumberTriviaModel;
+
+    // This setUp applies only to the 'device is online' group
+    setUp(() {
+      when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+    });
+
+    test(
+      'should return remote data when the call to remote data source is successful',
+      () async {
+        // arrange
+        when(() => mockRemoteDataSource.getConcreteNumberTrivia(tNumber))
+            .thenAnswer((_) async => tNumberTriviaModel);
+
+        // act
+        final result =
+            await repository.getConcreteNumberTrivia(number: tNumber);
+
+        // assert
+        verify(() => mockRemoteDataSource.getConcreteNumberTrivia(tNumber));
+
+        expect(result, equals(Right(numberTrivia)));
+      },
     );
   });
 }
