be20f30e5b16ff4ce8b3d4ebf5a2c07e438289cb
Ilham Hadi Prabawa <ilhamprabawa9@gmail.com>
2022-06-28 22:11:52 +0700

    testing remote datasource and local datasource

--- lib/features/number_trivia/data/datasource/number_trivia_local_data_source.dart
+++ lib/features/number_trivia/data/datasource/number_trivia_local_data_source.dart
@@ -1,7 +1,36 @@
+import 'dart:convert';
 import 'package:number_trivia_app/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:number_trivia_app/features/number_trivia/domain/entity/number_trivia.dart';
+import 'package:number_trivia_app/triviakit/error/exception.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const CACHED_NUMBER_TRIVIA = 'CACHED_NUMBER_TRIVIA';
 
 abstract class NumberTriviaLocalDataSource {
   Future<NumberTriviaModel> getLastNumberTrivia();
   Future<void> cacheNumberTrivia(NumberTrivia triviaToCache);
+}
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
+  NumberTriviaLocalDataSourceImpl({required this.sharedPreferences});
+
+  SharedPreferences sharedPreferences;
+
+  @override
+  Future<void> cacheNumberTrivia(NumberTrivia triviaToCache) {
+    return sharedPreferences.setString(
+      CACHED_NUMBER_TRIVIA,
+      json.encode(triviaToCache),
+    );
+  }
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() {
+    final jsonString = sharedPreferences.getString(CACHED_NUMBER_TRIVIA);
+    if (jsonString != null) {
+      return Future.value(NumberTriviaModel.fromJson(json.decode(jsonString)));
+    } else {
+      throw CacheException();
+    }
+  }
 }
--- lib/features/number_trivia/data/datasource/number_trivia_remote_datasource.dart
+++ lib/features/number_trivia/data/datasource/number_trivia_remote_datasource.dart
@@ -1,6 +1,37 @@
+import 'dart:convert';
+
 import 'package:number_trivia_app/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:number_trivia_app/triviakit/error/exception.dart';
 
 abstract class NumberTriviaRemoteDataSource {
   Future<NumberTriviaModel> getConcreteNumberTrivia(int? number);
   Future<NumberTriviaModel> getRandomNumberTrivia();
+}
+
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
+  final http.Client client;
+
+  NumberTriviaRemoteDataSourceImpl({required this.client});
+
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int? number) =>
+      _getFromUrl('http://numbersapi.com/$number');
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() =>
+      _getFromUrl('http://numbersapi.com/random');
+
+  Future<NumberTriviaModel> _getFromUrl(String url) async {
+    final response = await client.get(
+      Uri.parse(url),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      return NumberTriviaModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
 }
